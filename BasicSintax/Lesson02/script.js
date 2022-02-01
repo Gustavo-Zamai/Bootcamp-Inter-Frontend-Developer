605ced27dae38573d740cab18d8c8339f02b1e81
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-01-31 22:26:20 -0300

    Lesson02

---
+++ BasicSintax/Lesson02/script.js
@@ -0,0 +1,12 @@
+//primitive types
+//boolean = True or False
+var tOrF = false;//boolean
+
+//number = integer and double, float
+var number = 1;//number = integer
+var floatNumber = 0.5;//number = float/double
+
+//string
+var name = 'Gus'// string
+var letter = 'G'// Char
+
