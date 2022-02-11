797065d4bd6cc61b89ed2af05cfeb22757de0c51
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-02-11 20:59:44 -0300

    Lesson about DOM

---
+++ BaseCampJS/DOM/DOM.js
@@ -0,0 +1,6 @@
+/*Atividade: Light Mode/Dark Mode
+Crie uma estrutura básica de projeto front-end (HTML, CSS e JS)
+Copie o HTML e o CSS deste repositório
+Crie um arquivo chamado scripts.js na sua pasta assets/js
+Selecione os elementos: h1, button, footer e body
+Se os elementos possuírem a classe dark-mode, modifique seus estilos. Caso contrário, volte os estilos para o original*/
